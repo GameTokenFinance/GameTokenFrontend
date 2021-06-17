aa937e3f309e683b379c186144259df19765cc2c
GameTokenFinance <info@gametoken.finance>
2021-06-17 18:10:49 +0300

    b

--- src/utils/callHelpers.ts
+++ src/utils/callHelpers.ts
@@ -1,5 +1,5 @@
 import BigNumber from 'bignumber.js'
-import { DEFAULT_GAS_LIMIT, DEFAULT_TOKEN_DECIMAL } from 'config'
+import { DEFAULT_TOKEN_DECIMAL } from 'config'
 import { ethers } from 'ethers'
 
 import { getLpContract, getMasterchefContract } from 'utils/contractHelpers'
